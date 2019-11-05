a88a1b159525baa6ead2f52a999221e8bc735559
Alexis Coulombe <acoulombe@globalia.ca>
2019-11-05 14:21:17 -0500

    Responsive fix

--- resources/views/channel/index.blade.php
+++ resources/views/channel/index.blade.php
@@ -45,8 +45,11 @@
                             </div>
                         @endforeach
                     @else
-                        <div class="col-sm-1 col-md-2"></div>
-                        <div class="col-sm-3 col-md-3">
+                        <div class="col-md-3">
+                            <h2>Empty !</h2>
+                            <p>This author does not have any content !</p>
+                        </div>
+                        <div class="col-md-9">
                             <svg width="380px" height="500px" viewBox="0 0 837 1045" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
                                 <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
                                     <path d="M353,9 L626.664028,170 L626.664028,487 L353,642 L79.3359724,487 L79.3359724,170 L353,9 Z" id="Polygon-1" stroke="#FF0000" stroke-width="6" sketch:type="MSShapeGroup"></path>
@@ -56,10 +59,6 @@
                                     <path d="M281,801 L383,861.025276 L383,979.21169 L281,1037 L179,979.21169 L179,861.025276 L281,801 Z" id="Polygon-5" stroke="#FF4747" stroke-width="6" sketch:type="MSShapeGroup"></path>
                                 </g>
                             </svg>
-                        </div>
-                        <div class="col-sm-7 col-md-7">
-                            <h2>Empty !</h2>
-                            <p>This author does not have any content !</p>
                         </div>
                     @endif
                 </div>
