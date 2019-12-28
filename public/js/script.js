6d21e6e701e96c34208a91c17ce099c9aae78682
Chargnn <alexis.410@hotmail.com>
2019-12-28 17:04:23 -0500

    Untracked files

--- public/js/script.js
+++ public/js/script.js
@@ -175,4 +175,24 @@ Version: 1.0
         $('.sidebar.navbar-nav').addClass('toggled');
         $(this).toggle();
     });
+
+    // Classic editor init
+    if ($('#editor').length) {
+        ClassicEditor.create(document.querySelector('#editor'), {
+            toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'],
+            heading: {
+                options: [
+                    {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+                    {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
+                    {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'}
+                ]
+            }
+        });
+    }
+
+    // Comment filter trigger
+    $('.filter_item').click(function () {
+        $('#filter_value').val($(this).data('value'));
+        $('#filter_form').submit();
+    });
 })(jQuery); // End of use strict
--- resources/views/channel/about.blade.php
+++ resources/views/channel/about.blade.php
@@ -14,15 +14,46 @@
         <div class="container-fluid">
             <div class="video-block section-padding">
                 <div class="row">
-                    <h1 class="h2 col-lg-12">About {{ $author->getName() }}</h1>
-                    <hr>
                     <div class="col-lg-6">
-                        @if($setting)
-                            {!! $setting->getAbout() !!}
-                        @else
-                            No description provided.
-                        @endif
+                        <div class="single-video-title box mb-3">
+                            <h1 class="h2">About {{ $author->getName() }}</h1>
+                            <p class="mb-0">
+                                <i class="fas fa-edit"></i> <a href="#" data-toggle="modal" data-target="#editAboutModal">Edit your description</a>
+                            </p>
+                            <hr>
+                            <p>
+                                @if($setting)
+                                    {!! $setting->getAbout() !!}
+                                @else
+                                    No description provided.
+                                @endif
+                            </p>
+                            <br>
+                        </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="editAboutModal" tabindex="-1" role="dialog" aria-labelledby="editAboutModal" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit your description</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" class="form" id="form">
+                        {{ csrf_field() }}
+                        <textarea name="about" id="editor" class="col-lg-12">{{ $setting ? $setting->getAbout() : 'No description provided.' }}</textarea>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" type="button" data-dismiss="modal">Cancel</button>
+                    <button class="btn btn-success" type="submit" data-dismiss="modal" onclick="$('#form').submit();">Save</button>
                 </div>
             </div>
         </div>
