88ec591facbbb44521fc65935f7375e4b75e64b8
Alexis Coulombe <acoulombe@globalia.ca>
2020-01-21 14:49:50 -0500

    Fix category links

--- database/seeds/categories_list.sql
+++ database/seeds/categories_list.sql
@@ -1,13 +1,13 @@
 INSERT INTO 7vid.categories (`title`, `icon`)
 VALUES
-('Science & Technology','fa-flask'),
-('Howto & Style','fa-wrench'),
-('Comedy','fa-theater-masks'),
-('People & Blogs','fa-comment-alt'),
-('Gaming','fa-hat-wizard'),
-('Travel & Events','fa-plane'),
-('Sports','fa-quidditch'),
-('Pets & Animals','fa-paw'),
-('Music','fa-music'),
-('Autos & Vehicles','fa-car'),
-('Film & Animation','fa-film');
+('Science & Technology', 'science-technology', 'fa-flask'),
+('Howto & Style', 'howto-stlye', 'fa-wrench'),
+('Comedy', 'comedy', 'fa-theater-masks'),
+('People & Blogs', 'people-blogs', 'fa-comment-alt'),
+('Gaming', 'gaming', 'fa-hat-wizard'),
+('Travel & Events', 'travel-events', 'fa-plane'),
+('Sports', 'sports', 'fa-quidditch'),
+('Pets & Animals', 'pets-animals', 'fa-paw'),
+('Music', 'music', 'fa-music'),
+('Autos & Vehicles', 'auto-vehicles', 'fa-car'),
+('Film & Animation', 'film-animation', 'fa-film');
