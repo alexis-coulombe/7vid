17c56f31f6a741fc9bdd529aae1b0a5cad4dacb8
Chargnn <alexis.410@hotmail.com>
2020-01-21 20:15:32 -0500

    Updated video uploading page

--- resources/views/video/create.blade.php
+++ resources/views/video/create.blade.php
@@ -1,37 +1,69 @@
 @extends('shared.template')
 
-@section('body-class')
-    "landing-page"
+@section('title')
+    Upload a video
+@endsection
+
+@section('description')
+    Upload a video on 7-Vid in less then 2 minutes.
 @endsection
 
 @section('content')
     <div class="row">
         <div class="col-lg-12">
             <div class="main-title">
-                <h6>Upload Details</h6>
+                <h1 class="h2">Upload a video in 3 step.</h1>
             </div>
         </div>
     </div>
     <hr>
     <div class="row">
-        <div class="col-lg-12">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
             <form action="{{ route('video.store') }}" method="POST" enctype="multipart/form-data">
                 {{ csrf_field() }}
 
-                <div class="osahan-form">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="form-group">
-                                <label for="e1">Video file</label>
-                                <input type="file" name="upload" id="e1" class="form-control-file">
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="form-group">
-                                <label for="e1">Thumbnail</label>
-                                <input type="file" name="image" id="e1" class="form-control-file">
-                            </div>
-                        </div>
+                <div class="form-group col-lg-12 mx-auto">
+                    <div class="p-5 bg-white shadow rounded-lg">
+                        <h1 class="text-center">
+                            <i class="fas fa-cloud-upload-alt"></i>
+                        </h1>
+
+                        <h5 class="text-center mb-4">
+                            <b>1. Upload your video</b>
+                        </h5>
+                        <p class="text-center text-muted">
+                            A thumbnail will be generated once you have selected your video
+                        </p>
+
+                        <label for="e1" class="file-upload btn btn-primary btn-block rounded-pill shadow">
+                            <i class="fa fa-upload mr-2"></i>
+                            Browse for file ...
+                            <input type="file" name="upload" id="e1" class="form-control-file d-none">
+                        </label>
+                        <hr>
+                        <h6 class="text-center mb-4">
+                            <b>Not happy with the generated thumbnail ?</b>
+                        </h6>
+                        <p class="text-center text-muted">
+                            Upload your own
+                        </p>
+                        <label for="e4" class="file-upload btn btn-primary btn-block rounded-pill shadow">
+                            <i class="fa fa-upload mr-2"></i>
+                            Browse for file ...
+                            <input type="file" name="image" id="e4" class="form-control-file d-none">
+                        </label>
+                    </div>
+                </div>
+                <div class="form-group col-lg-12 mx-auto">
+                    <div class="p-5 bg-white shadow rounded-lg">
+                        <h1 class="text-center">
+                            <i class="fas fa-clipboard"></i>
+                        </h1>
+
+                        <h5 class="text-center mb-4">
+                            <b>2. What is your video about ?</b>
+                        </h5>
                         <div class="col-lg-12">
                             <div class="form-group">
                                 <label for="e1">Video Title</label>
@@ -44,9 +76,7 @@
                                 <textarea rows="3" id="e2" name="description" class="form-control" placeholder="A short description"></textarea>
                             </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-3">
+                        <div class="col-lg-12">
                             <div class="form-group">
                                 <label for="e3">Category</label>
                                 <select id="e3" class="custom-select" name="category">
@@ -59,44 +89,61 @@
                         </div>
                     </div>
                 </div>
-                <h5>Other settings</h5>
-                <hr>
-                <label class="toggle-check">
-                    <input type="checkbox" name="private" class="toggle-check-input">
-                    <span class="toggle-check-text"></span>
-                    Mark video has hidden
-                    <span title="" data-placement="top" data-toggle="tooltip" data-original-title="Nobody, but you, will be able to see this video.">
-                        <i class="far fa-question-circle"></i>
-                    </span>
-                </label>
+                <div class="form-group col-lg-12 mx-auto">
+                    <div class="p-5 bg-white shadow rounded-lg">
+                        <h1 class="text-center">
+                            <i class="fas fa-filter"></i>
+                        </h1>
 
-                <label class="toggle-check">
-                    <input type="checkbox" name="allow_comments" class="toggle-check-input" checked>
-                    <span class="toggle-check-text"></span>
-                    Allow comments
-                    <span title="" data-placement="top" data-toggle="tooltip" data-original-title="People will be able to comment on your video.">
-                        <i class="far fa-question-circle"></i>
-                    </span>
-                </label>
+                        <h5 class="text-center mb-4">
+                            <b>3. Optional settings</b>
+                        </h5>
 
-                <label class="toggle-check">
-                    <input type="checkbox" name="allow_votes" class="toggle-check-input" checked>
-                    <span class="toggle-check-text"></span>
-                    Allow ratings
-                    <span title="" data-placement="top" data-toggle="tooltip" data-original-title="People will be able to rate this video.">
-                        <i class="far fa-question-circle"></i>
-                    </span>
-                </label>
-            <div class="osahan-area text-center mt-3">
-                @include('shared.captcha.recaptcha')
-                <button type="submit" class="btn btn-outline-primary">Share !</button>
-            </div>
+                        <div class="col-lg-12">
+                            <label class="toggle-check">
+                                <input type="checkbox" name="private" class="toggle-check-input">
+                                <span class="toggle-check-text"></span>
+                                Mark video has hidden
+                                <span title="" data-placement="top" data-toggle="tooltip" data-original-title="Nobody, but you, will be able to see this video.">
+                                    <i class="far fa-question-circle"></i>
+                                </span>
+                            </label>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <label class="toggle-check">
+                                <input type="checkbox" name="allow_comments" class="toggle-check-input" checked>
+                                <span class="toggle-check-text"></span>
+                                Allow comments
+                                <span title="" data-placement="top" data-toggle="tooltip" data-original-title="People will be able to comment on your video.">
+                                    <i class="far fa-question-circle"></i>
+                                </span>
+                            </label>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <label class="toggle-check">
+                                <input type="checkbox" name="allow_votes" class="toggle-check-input" checked>
+                                <span class="toggle-check-text"></span>
+                                Allow ratings
+                                <span title="" data-placement="top" data-toggle="tooltip" data-original-title="People will be able to rate this video.">
+                                    <i class="far fa-question-circle"></i>
+                                </span>
+                            </label>
+                        </div>
+
+                        <div class="text-center">
+                            @include('shared.captcha.recaptcha')
+                            <button type="submit" class="btn btn-outline-primary">Share !</button>
+                        </div>
+                    </div>
+                </div>
             </form>
-            <hr>
-            <div class="terms text-center">
-                <p class="mb-0">There are many variations of passages of Lorem Ipsum available, but the majority <a href="#">Terms of Service</a> and <a href="#">Community Guidelines</a>.</p>
-                <p class="hidden-xs mb-0">Ipsum is therefore always free from repetition, injected humour, or non</p>
-            </div>
         </div>
+    </div>
+    <div class="terms text-center mt-2">
+        <p class="mb-0">
+            By uploading videos to {{ config('app.name') }}, you agree to the <a href="{{ route('home.terms') }}">Terms of Service</a> and Community Rules.
+            Be careful not to infringe the copyright or privacy of others.</p>
     </div>
 @endsection
