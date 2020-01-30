373a1e5a4fa5dcd6501f2d4d9b447fd565a7358b
Alexis Coulombe <acoulombe@globalia.ca>
2020-01-30 15:38:23 -0500

    Set cookie consent on alert close

--- resources/views/shared/footer/cookie-consent.blade.php
+++ resources/views/shared/footer/cookie-consent.blade.php
@@ -1,5 +1,5 @@
-<div id="lawmsg" class="alert alert-info alert-dismissible h4 show fixed-bottom" role="alert" onload="setCookie('cookie-consent', true)">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+<div id="lawmsg" class="alert alert-info alert-dismissible h4 show fixed-bottom" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="setCookie('cookie-consent', true)">
         <span aria-hidden="true">&times;</span>
     </button>
     We use cookies on this website to distinguish you from other users. &nbsp; We use this data to enhance your experience and for targeted advertising. &nbsp; By continuing to use this website you consent to our use of cookies. &nbsp;
