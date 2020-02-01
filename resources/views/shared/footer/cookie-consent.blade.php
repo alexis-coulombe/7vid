30a308160a9f7a85a2e92bb5db55c01bd17fbbf7
Chargnn <alexis.410@hotmail.com>
2020-02-01 14:48:50 -0500

    Fixed cookie consent alert

--- resources/views/shared/footer/cookie-consent.blade.php
+++ resources/views/shared/footer/cookie-consent.blade.php
@@ -1,5 +1,5 @@
-<div id="lawmsg" class="alert alert-info alert-dismissible h4 show fixed-bottom" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="setCookie('cookie-consent', true)">
+<div id="lawmsg" class="alert alert-info alert-dismissible h4 d-none fixed-bottom" role="alert">
+    <button id="consent-cookie" type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
     We use cookies on this website to distinguish you from other users. &nbsp; We use this data to enhance your experience and for targeted advertising. &nbsp; By continuing to use this website you consent to our use of cookies. &nbsp;
